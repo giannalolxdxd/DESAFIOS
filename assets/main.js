49e5209a2f5162ce9e76f291179f62b84c261aa4
Giiifz <giannauwu12@gmail.com>
2024-09-20 19:50:59 -0300

    nav y java

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+alert ("Hola desde main.js");
+console.log("Hola desde main.js")
+console.error("error");
+
+let X = 10;
+const Y = 5; 
+
+let nombre = "Gianna";
+let edad = 16;
+X = 15;
+
+console.log(X+Y);
+console.log(X,Y);
+console.log("Tu nombre es: " + nombre + "Y tu edad es. " + edad);
+console.log(`Tu nombre es:${nombre} y tu edad es: ${edad}`);
+
+const NU = prompt('ingrese su nombre')
+
+console.log(NU)
